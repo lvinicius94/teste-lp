8c88fc4e96dbb87fb929aa6db9fc1344f6bcde2e
Lucas <lvinicius94@hotmail.com>
2024-02-08 12:09:49 -0300

    Ajuste imagens e caminhos

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ function App() {
 
   return (
       <div className='div-principal-lp'>
-        <div className='menu-principal'><img src="src/logo-upbet.png" alt="" /><button className='botao-cadastrar-menu'>CADASTRE-SE</button></div>
+        <div className='menu-principal'><img src="/logo-upbet.png" alt="" /><button className='botao-cadastrar-menu'>CADASTRE-SE</button></div>
         <div className='banner-imagem-texto'>
               <div className='conteudo-banner'>
                   <p>Exclusiva para novos clientes!</p>
                   <h1>200 RODADAS GRÁTIS</h1>
                   <h2>PARA JOGAR!</h2>
                   <div className='config-botao-banner'>
-                      <img className='icone-botao-menu' src="src/icone-png.png" alt="" />
+                      <img className='icone-botao-menu' src="/icone-png.png" alt="" />
                       <button className='botao-cadastrar-banner'>GANHAR BÔNUS</button>
-                     <img className='icone-botao-menu' src="src/icone-png.png" alt="" /> 
+                     <img className='icone-botao-menu' src="/icone-png.png" alt="" /> 
                      <p>Termos e Condições se aplicam a essa promoção</p>
                   </div>
               </div>
