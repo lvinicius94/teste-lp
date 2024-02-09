2ab6b90762c5928887980d733b6da08bd8857349
Lucas <lvinicius94@hotmail.com>
2024-02-09 18:29:31 -0300

    Ajuste final lp 20

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,54 @@
 import { useState } from 'react'
 import './App.css'
+import PromoCode from './PromoCode'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div className='div-principal-lp'>
-        <div className='menu-principal'><img src="/logo-principal-upbet.png" alt="" /><button className='botao-cadastrar-menu'>CADASTRE-SE</button></div>
+        <div className='menu-principal'><img src="/logo-principal-upbet.png" alt="" /><button className='botao-cadastrar-menu'><a href="https://upbet.com/ptb/bet/main?affid=0209103">CADASTRE-SE</a></button></div>
         <div className='banner-imagem-texto'>
               <div className='conteudo-banner'>
                   <div className='conteudo-topo'>
-                  <p>Exclusiva para novos clientes!</p>
-                  <h1>200 RODADAS GRÁTIS</h1>
+                  <p>É isso mesmo galera, voce deposita apenas 5 reais e vai ganhar 20 reais!!! </p>
+                  <h1><b>GANHE R$ 20,00</b>, depositando APENAS R$ 5,00!!!</h1>
+                 
                   </div>
                   <div className='config-botao-banner'>
-                  <h2>PARA JOGAR!</h2>
-                      <img className='icone-botao-menu' src="/icone-png.png" alt="" />
-                      <button className='botao-cadastrar-banner'>GANHAR BÔNUS</button>
-                     <img className='icone-botao-menu' src="/icone-png.png" alt="" /> 
-                     <p>Termos e Condições se aplicam a essa promoção</p>
+                    
+                      <button className='botao-cadastrar-banner'><a href="https://upbet.com/ptb/bet/main?affid=0209103">GANHAR BÔNUS</a></button>
+                      
+                     <p>Deposite agora com o "promocoudi" UPC24</p>
+                     <PromoCode />
                   </div>
               </div>
               <div className='imagem-banner-direita'>
-                  <img src="/fundo-gates-2.png" alt="" />
+                  <img src="/fundo-trio-fortune-dragon-fortune-rabbit-fortune-ox.png" alt="" />
               </div>
         </div>
 
 
         <div className='div-segunda-dobra'>
-            <h2>Promoções</h2>
-            <h3>BÔnus de 1° Primeiro Depósito</h3>
-            <div className='card-list'>
-                <div className='card-one'>
-                    <h4>5 RODADAS GRÁTIS</h4>
-                    <p>DEPOSITE R$ 5,00</p>
-                </div>            
-                <img className='icone-botao-menu-2' src="/icone-png.png" alt="" /> 
-                <div className='card-one'>
-                    <h4>5 RODADAS GRÁTIS</h4>
-                    <p>DEPOSITE R$ 5,00</p>
-                </div>            
-                <img className='icone-botao-menu-2' src="/icone-png.png" alt="" /> 
-                <div className='card-one'>
-                    <h4>5 RODADAS GRÁTIS</h4>
-                    <p>DEPOSITE R$ 5,00</p>
-                </div>            
-                <img className='icone-botao-menu-2' src="/icone-png.png" alt="" /> 
-                <div className='card-one'>
-                    <h4>5 RODADAS GRÁTIS</h4>
-                    <p>DEPOSITE R$ 5,00</p>
-                </div>            
-                <img className='icone-botao-menu-2' src="/icone-png.png" alt="" /> 
-                <div className='card-one'>
-                    <h4>5 RODADAS GRÁTIS</h4>
-                    <p>DEPOSITE R$ 5,00</p>
-                </div>
                 <div className='texto-final'>
                     <h2>Como Receber</h2>
-                    <h3>1 - Crie a sua conta</h3>
-                    <p>O cadastro é rápido, leva apenas 2 minutos!</p>
+                    <h3>1 - Deposite 5,00 e ganhe 15,00 usando o promocode na tela.</h3>
+                    <p>Clique no botão para garantir seu bônus.</p>
 
-                    <h3>2 - faça um depósito via Pix</h3>
-                    <p>Valor mínimo de R$ 5,00 reais.</p>    
+                    <h3>2 - Deposite 5 e ganhe 15 usando o promocode que está na tela.</h3>
+                    <p>Clique no botão e aproveite. </p>    
 
-                    <h3>3 - Receba o seu bônus</h3>
-                    <p>Receba rodadas grátis de acordo com o valor do primeiro depósito</p>  
+                    <h3>3 - Deposite 5 e ganhe 3x o valor digite o promocode na página de depósito.</h3>
+                    <p>Clique no botão para garantir seu bônus.</p>  
 
-                    <h3>4 - Complete os requisitos de aposta</h3>
-                    <span>Ganhe 5 rodadas grátis depositando 5 reais</span>
-                    <span>Ganhe 20 rodadas grátis depositando 20 reais</span>
-                    <span>ganhe 50 rodadas grátis depositando 50 reais</span>
-                    <span>ganhe 100 rodadas grátis depositando 100 reais</span>
-                    <span>ganhe 200 rodadas grátis depositando 500 reais</span>      
+                    <h3>4 - Ganhe 15 reais de bônus depositando 5,00 usando o promocode que está na tela.</h3>
+                    <p>Clique no botão para aproveitar.</p>
+
+                    <h3>5 - Use o código para ganhar 15,00 de bônus!</h3>
+                    <p>Clique no botão e aproveite.</p>   
                 </div>
             </div>
         </div>
-
-      </div>
 
   )
 }
---
+++ src/PromoCode.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './PromoCode.css'
+
+function PromoCode() {
+  const [copied, setCopied] = useState(false);
+  const promoCode = 'UPC24';
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(promoCode)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000); // Reseta o estado de 'copiado' após 2 segundos
+      })
+
+  };
+
+  return (
+    <div>
+      <button className='botao-codigo-promocional' onClick={handleCopyClick}>
+        {copied ? 'CÓDIGO COPIADO!' : 'COPIAR PROMOCODE'}
+      </button>
+    </div>
+  );
+}
+
+export default PromoCode;
